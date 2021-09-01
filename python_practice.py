810d38a034bd14af6245794a365aeb098b6a3e79
nabilram <89229716+nabilram@users.noreply.github.com>
2021-08-31 18:30:10 -0700

    3.2.9

--- python_practice.py
+++ python_practice.py
@@ -1,3 +1,18 @@
 counties = ["Arapahoe","Denver","Jefferson"]
 if counties[1] == 'Denver':
     print(counties[1])
+    
+## What is the score?
+score = int(input("What is your test score? "))
+
+## Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
